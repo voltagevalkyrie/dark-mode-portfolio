8d3c1f9e8a0de4901245de428deda6f551bc5542
valkyrie <149293731+voltagevalkyrie@users.noreply.github.com>
2024-01-18 22:42:31 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function () {
+    // Your JavaScript code goes here
+
+    
+    let btn = document.querySelector(".darkmode");
+
+btn.addEventListener("click", () => {
+  if (btn.innerHTML === `<i class="fas fa-moon"></i>`) {
+    btn.innerHTML = `<i class="fas fa-sun"></i>`;
+    btn.style.color = "orange";
+    btn.style.backgroundColor = "rgb(43, 43, 43)";
+    document.body.classList.add("darkmode");
+  } else {
+    btn.innerHTML = `<i class="fas fa-moon"></i>`;
+    btn.style.color = "black";
+    btn.style.backgroundColor = "white";
+    document.body.classList.remove("darkmode");
+  }
+});
+
+
+//ends here 
+
+
+
+});
